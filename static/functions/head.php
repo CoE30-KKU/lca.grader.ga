40ff27b1a44d7a7728f1cc6b486188aee97c9c75
PondJaᵀᴴ <palapon2545@gmail.com>
2021-03-11 22:47:35 +0700

    Reset Password! ⛑

--- static/functions/head.php
+++ static/functions/head.php
@@ -14,14 +14,14 @@
     <link rel="apple-touch-icon" sizes="152x152" href="../static/elements/logo/logo_ios152.png">
 
     <?php $current_url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"; ?>
-    <meta property="og:image" content="//grader.ga/static/elements/logo/logo.jpg" />
+    <meta property="og:image" content="//lca.grader.ga/static/elements/logo/logo.jpg" />
     <meta property="og:image:width" content="194" />
     <meta property="og:image:height" content="194" />
     <meta property="og:title" content="Grader.GA - LCA Edition" />
     <title>Grader.ga - LCA Edition</title>
     <meta property="og:description" content="The Computer Engineering of Khon Kaen University Student-Made grader." />
     <meta name="twitter:card" content="summary"></meta>
-    <link rel="image_src" href="//grader.ga/static/elements/logo/logo.jpg" />
+    <link rel="image_src" href="//lca.grader.ga/static/elements/logo/logo.jpg" />
     <meta property="og:type" content="website" />
     <meta property="og:url" content="<?php echo $current_url; ?>" />
 
