3fab13a8d9b117c5480bd2e906161352ddeaa3ee
PondJaᵀᴴ <palapon2545@gmail.com>
2021-03-19 14:05:38 +0700

    *

--- pages/problem_save.php
+++ pages/problem_save.php
@@ -2,15 +2,6 @@
     require_once '../static/functions/connect.php';
     require_once '../static/functions/function.php';
 
-    function generateRandom($length = 5) {
-        $characters = md5(time());
-        $randomString = '';
-        for ($i = 0; $i < $length; $i++) {
-            $randomString .= $characters[rand(0, strlen($characters) - 1)];
-        }
-        return $randomString;
-    }
-
     $id = "";
     if (isLogin()) {
         if (isset($_POST['problem'])) {
